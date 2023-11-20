adec5f78f4531e9df913f16cb34ce245eda19e2e
Leo Galleguillos <leo.galleguillos@icloud.com>
2023-11-20 20:24:06 +0000

    Initial commit

---
+++ config/modules.config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * List of enabled modules for this application.
+ *
+ * This should be an array of module namespaces used in the application.
+ */
+return [
+	'MonthlyBasis\LaminasOpenAi',
+];
---
+++ src/Module.php
@@ -0,0 +1,15 @@
+<?php
+namespace MonthlyBasis\LaminasOpenAi;
+
+class Module
+{
+    public function getConfig()
+    {
+        return [];
+    }
+
+    public function getServiceConfig()
+    {
+        return [];
+    }
+}
